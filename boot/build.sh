371b77ac9a59ffe00e9a0979e35a32aeae30c57d
Timur <stukantimur811@gmail.com>
2025-04-20 14:47:34 +0300

    build file to create bit files from boot and loader and build boot part

---
+++ boot/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p "$SCRIPT_DIR/../build/boot"
+
+dd if=/dev/zero of="$SCRIPT_DIR/../build/boot.img" bs=512 count=20480
+nasm -f bin -o "$SCRIPT_DIR/../build/boot/boot.bin" "$SCRIPT_DIR/src/boot.asm"
+nasm -f bin -o "$SCRIPT_DIR/../build/boot/loader.bin" "$SCRIPT_DIR/src/loader.asm"
+dd if="$SCRIPT_DIR/../build/boot/boot.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=1 conv=notrunc
+dd if="$SCRIPT_DIR/../build/boot/loader.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=5 seek=1 conv=notrunc
+
