370bdc440bb5294670f653d31d68767ddc7afbcd
Timur <stukantimur811@gmail.com>
2025-04-21 20:43:24 +0300

    added to build kernel file building and add that 100 secotrs to the disk.img

--- boot/build.sh
+++ boot/build.sh
@@ -8,6 +8,7 @@ mkdir -p "$SCRIPT_DIR/../build/boot"
 dd if=/dev/zero of="$SCRIPT_DIR/../build/boot.img" bs=512 count=20480
 nasm -f bin -o "$SCRIPT_DIR/../build/boot/boot.bin" "$SCRIPT_DIR/src/boot.asm"
 nasm -f bin -o "$SCRIPT_DIR/../build/boot/loader.bin" "$SCRIPT_DIR/src/loader.asm"
+nasm -f bin -o "$SCRIPT_DIR/../build/boot/kernel.bin" "$SCRIPT_DIR/../kernel/src/kernel.asm"
 dd if="$SCRIPT_DIR/../build/boot/boot.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=1 conv=notrunc
 dd if="$SCRIPT_DIR/../build/boot/loader.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=5 seek=1 conv=notrunc
-
+dd if="$SCRIPT_DIR/../build/boot/kernel.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=100 seek=6 conv=notrunc
