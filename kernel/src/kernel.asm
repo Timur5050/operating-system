1e57ee9031af8a6103995e11f395c4226138b403
Timur <stukantimur811@gmail.com>
2025-04-24 15:36:14 +0300

    added spurios interrupts handling and created func to adding new IDT entries without overriding the same block of code

--- kernel/src/kernel.asm
+++ kernel/src/kernel.asm
@@ -4,21 +4,16 @@
 start:
     mov rdi,Idt
     mov rax,Handler0
-    mov [rdi],ax
-    shr rax,16
-    mov [rdi+6],ax
-    shr rax,16
-    mov [rdi+8],eax
+    call SetHandler
 
     mov rax,Timer
-    add rdi,32*16
-    mov [rdi],ax
-    shr rax,16
-    mov [rdi+6],ax
-    shr rax,16
-    mov [rdi+8],eax
+    mov rdi,Idt+32*16
+    call SetHandler
 
-
+    mov rdi,Idt+32*16+7*16
+    mov rax,SIRQ
+    call SetHandler
+    
     lgdt [Gdt64Ptr]
     lidt [IdtPtr]
 
@@ -86,6 +81,14 @@ InitPIC:
 End:
     hlt
     jmp End
+
+SetHandler:
+    mov [rdi],ax
+    shr rax,16
+    mov [rdi+6],ax
+    shr rax,16
+    mov [rdi+8],eax
+    ret
 
 UserEntry:
     inc byte[0xb8010]
@@ -172,6 +175,53 @@ Timer:
     pop	rax
 
     iretq
+
+SIRQ:
+    push rax
+    push rbx  
+    push rcx
+    push rdx  	  
+    push rsi
+    push rdi
+    push rbp
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+
+    mov al,11
+    out 0x20,al
+    in al,0x20
+
+    test al,(1<<7)
+    jz .end
+
+    mov al,0x20
+    out 0x20,al
+
+.end:
+    pop	r15
+    pop	r14
+    pop	r13
+    pop	r12
+    pop	r11
+    pop	r10
+    pop	r9
+    pop	r8
+    pop	rbp
+    pop	rdi
+    pop	rsi  
+    pop	rdx
+    pop	rcx
+    pop	rbx
+    pop	rax
+
+    iretq
+
 
 Gdt64:
     dq 0
