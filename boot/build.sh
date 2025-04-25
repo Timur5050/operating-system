55daee5d3f82138630bacbb98a4a323868f132d4
Timur <stukantimur811@gmail.com>
2025-04-25 23:51:41 +0300

    added trap, lib to kernel file to more functionality

--- boot/build.sh
+++ boot/build.sh
@@ -9,10 +9,15 @@ dd if=/dev/zero of="$SCRIPT_DIR/../build/boot.img" bs=512 count=20480
 nasm -f bin -o "$SCRIPT_DIR/../build/boot/boot.bin" "$SCRIPT_DIR/src/boot.asm"
 nasm -f bin -o "$SCRIPT_DIR/../build/boot/loader.bin" "$SCRIPT_DIR/src/loader.asm"
 nasm -f elf64 -o "$SCRIPT_DIR/../build/kernel/kernel.o" "$SCRIPT_DIR/../kernel/src/core/kernel.asm"
+nasm -f elf64 -o "$SCRIPT_DIR/../build/kernel/trapa.o" "$SCRIPT_DIR/../kernel/src/interrupts/trap.asm"
+nasm -f elf64 -o "$SCRIPT_DIR/../build/kernel/liba.o" "$SCRIPT_DIR/../kernel/src/lib/lib.asm"
 gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone \
     -c "$SCRIPT_DIR/../kernel/src/core/main.c" -o "$SCRIPT_DIR/../build/kernel/main.o"
+gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone \
+    -c "$SCRIPT_DIR/../kernel/src/interrupts/trap.c" -o "$SCRIPT_DIR/../build/kernel/trap.o"
 ld -nostdlib -T "$SCRIPT_DIR/../kernel/src/link.lds" -o "$SCRIPT_DIR/../build/kernel/kernel.elf" \
-    "$SCRIPT_DIR/../build/kernel/kernel.o" "$SCRIPT_DIR/../build/kernel/main.o"
+    "$SCRIPT_DIR/../build/kernel/kernel.o" "$SCRIPT_DIR/../build/kernel/main.o" \
+    "$SCRIPT_DIR/../build/kernel/trapa.o" "$SCRIPT_DIR/../build/kernel/trap.o" "$SCRIPT_DIR/../build/kernel/liba.o"
 objcopy -O binary "$SCRIPT_DIR/../build/kernel/kernel.elf" "$SCRIPT_DIR/../build/kernel/kernel.bin"
 dd if="$SCRIPT_DIR/../build/boot/boot.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=1 conv=notrunc
 dd if="$SCRIPT_DIR/../build/boot/loader.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=5 seek=1 conv=notrunc
