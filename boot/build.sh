e62a4fbe5fb65e48a5ca20df0a7b3bdd1d661d46
Timur <stukantimur811@gmail.com>
2025-04-25 15:13:20 +0300

    added compilation of main.c to object file and kernel.asm to elf file to link them together and get obejct file and then convert it to binary and push to the virtual disk

--- boot/build.sh
+++ boot/build.sh
@@ -8,7 +8,12 @@ mkdir -p "$SCRIPT_DIR/../build/boot"
 dd if=/dev/zero of="$SCRIPT_DIR/../build/boot.img" bs=512 count=20480
 nasm -f bin -o "$SCRIPT_DIR/../build/boot/boot.bin" "$SCRIPT_DIR/src/boot.asm"
 nasm -f bin -o "$SCRIPT_DIR/../build/boot/loader.bin" "$SCRIPT_DIR/src/loader.asm"
-nasm -f bin -o "$SCRIPT_DIR/../build/boot/kernel.bin" "$SCRIPT_DIR/../kernel/src/kernel.asm"
+nasm -f elf64 -o "$SCRIPT_DIR/../build/kernel/kernel.o" "$SCRIPT_DIR/../kernel/src/core/kernel.asm"
+gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone \
+    -c "$SCRIPT_DIR/../kernel/src/core/main.c" -o "$SCRIPT_DIR/../build/kernel/main.o"
+ld -nostdlib -T "$SCRIPT_DIR/../kernel/src/link.lds" -o "$SCRIPT_DIR/../build/kernel/kernel.elf" \
+    "$SCRIPT_DIR/../build/kernel/kernel.o" "$SCRIPT_DIR/../build/kernel/main.o"
+objcopy -O binary "$SCRIPT_DIR/../build/kernel/kernel.elf" "$SCRIPT_DIR/../build/kernel/kernel.bin"
 dd if="$SCRIPT_DIR/../build/boot/boot.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=1 conv=notrunc
 dd if="$SCRIPT_DIR/../build/boot/loader.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=5 seek=1 conv=notrunc
-dd if="$SCRIPT_DIR/../build/boot/kernel.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=100 seek=6 conv=notrunc
+dd if="$SCRIPT_DIR/../build/kernel/kernel.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=100 seek=6 conv=notrunc
