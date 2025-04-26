065cc6b8becc5c153e1ff323d3532b296c5b6417
Timur <stukantimur811@gmail.com>
2025-04-26 17:12:26 +0300

    testing assertion, and printing data by vga mode

--- kernel/src/core/main.c
+++ kernel/src/core/main.c
@@ -1,6 +1,13 @@
 #include "../interrupts/trap.h"
+#include "../print_function/print.h"
 
 void KMain(void)
 {
+   char *string = "Hello and Welcome";
+   int64_t value = 0x123456789ABCD;
+   
    init_idt();
+
+   printk("%s\n", string);
+   printk("This value is equal to %x", value);
 }
