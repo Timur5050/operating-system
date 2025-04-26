c36383d0d793979c2f940f5c1c9548d40b830222
Timur <stukantimur811@gmail.com>
2025-04-26 20:28:41 +0300

    added print file to whole compilation

--- boot/build.sh
+++ boot/build.sh
@@ -15,9 +15,12 @@ gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone \
     -c "$SCRIPT_DIR/../kernel/src/core/main.c" -o "$SCRIPT_DIR/../build/kernel/main.o"
 gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone \
     -c "$SCRIPT_DIR/../kernel/src/interrupts/trap.c" -o "$SCRIPT_DIR/../build/kernel/trap.o"
+gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone \
+    -c "$SCRIPT_DIR/../kernel/src/print_function/print.c" -o "$SCRIPT_DIR/../build/kernel/print.o"
 ld -nostdlib -T "$SCRIPT_DIR/../kernel/src/link.lds" -o "$SCRIPT_DIR/../build/kernel/kernel.elf" \
     "$SCRIPT_DIR/../build/kernel/kernel.o" "$SCRIPT_DIR/../build/kernel/main.o" \
-    "$SCRIPT_DIR/../build/kernel/trapa.o" "$SCRIPT_DIR/../build/kernel/trap.o" "$SCRIPT_DIR/../build/kernel/liba.o"
+    "$SCRIPT_DIR/../build/kernel/trapa.o" "$SCRIPT_DIR/../build/kernel/trap.o" \
+    "$SCRIPT_DIR/../build/kernel/liba.o" "$SCRIPT_DIR/../build/kernel/print.o"
 objcopy -O binary "$SCRIPT_DIR/../build/kernel/kernel.elf" "$SCRIPT_DIR/../build/kernel/kernel.bin"
 dd if="$SCRIPT_DIR/../build/boot/boot.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=1 conv=notrunc
 dd if="$SCRIPT_DIR/../build/boot/loader.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=5 seek=1 conv=notrunc
