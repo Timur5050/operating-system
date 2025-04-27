75b6280eee8131368451c8fad8baf131fe283675
Timur <stukantimur811@gmail.com>
2025-04-27 20:57:33 +0300

    added initiazliog of tables and getting all free blocks of memory (all tested)

--- kernel/src/core/main.c
+++ kernel/src/core/main.c
@@ -1,13 +1,9 @@
 #include "../interrupts/trap.h"
 #include "../print_function/print.h"
+#include "../memory_management/memory.h"
 
 void KMain(void)
-{
-   char *string = "Hello and Welcome";
-   int64_t value = 0x123456789ABCD;
-   
+{ 
    init_idt();
-
-   printk("%s\n", string);
-   printk("This value is equal to %x", value);
+   init_memory();
 }
