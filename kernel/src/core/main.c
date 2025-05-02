7941ce20fd5cc319b89ad4c3253d2793b37a2dd5
Timur <stukantimur811@gmail.com>
2025-05-02 15:03:32 +0300

    added func to create and launching process to test that mechanism

--- kernel/src/core/main.c
+++ kernel/src/core/main.c
@@ -1,10 +1,13 @@
 #include "../interrupts/trap.h"
 #include "../print_function/print.h"
 #include "../memory_management/memory.h"
+#include "../process/process.h"
 
 void KMain(void)
 { 
    init_idt();
    init_memory();  
    init_kvm();
+   init_process();
+   launch();
 }
