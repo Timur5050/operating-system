513e90231b60aded7dbb20db126f52a6d11a2636
Timur <stukantimur811@gmail.com>
2025-05-02 15:04:28 +0300

    added funcs to get cr2 register, where the processoe puts info about pages errors

--- kernel/src/interrupts/trap.asm
+++ kernel/src/interrupts/trap.asm
@@ -25,6 +25,8 @@ global eoi
 global read_isr
 global load_idt
 global load_cr3
+global pstart
+global read_cr2
 
 Trap:
     push rax
@@ -180,4 +182,12 @@ load_cr3:
     mov rax,rdi
     mov cr3,rax
     ret
+
+read_cr2:
+    mov rax,cr2
+    ret
+
+pstart:
+    mov rsp,rdi
+    jmp TrapReturn
 
