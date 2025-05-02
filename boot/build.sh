a55051c1aede8d7c5cea6052a8865512acc66049
Timur <stukantimur811@gmail.com>
2025-05-02 15:10:35 +0300

    compile proceess.c also and added it to the linker and kernel code in the virtual disk

--- boot/build.sh
+++ boot/build.sh
@@ -21,11 +21,14 @@ gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone \
     -c "$SCRIPT_DIR/../kernel/src/assertion/debug.c" -o "$SCRIPT_DIR/../build/kernel/debug.o"
 gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone \
     -c "$SCRIPT_DIR/../kernel/src/memory_management/memory.c" -o "$SCRIPT_DIR/../build/kernel/memory.o"
+gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone \
+    -c "$SCRIPT_DIR/../kernel/src/process/process.c" -o "$SCRIPT_DIR/../build/kernel/process.o"
 ld -nostdlib -T "$SCRIPT_DIR/../kernel/src/link.lds" -o "$SCRIPT_DIR/../build/kernel/kernel.elf" \
     "$SCRIPT_DIR/../build/kernel/kernel.o" "$SCRIPT_DIR/../build/kernel/main.o" \
     "$SCRIPT_DIR/../build/kernel/trapa.o" "$SCRIPT_DIR/../build/kernel/trap.o" \
     "$SCRIPT_DIR/../build/kernel/liba.o" "$SCRIPT_DIR/../build/kernel/print.o" \
-    "$SCRIPT_DIR/../build/kernel/debug.o" "$SCRIPT_DIR/../build/kernel/memory.o"
+    "$SCRIPT_DIR/../build/kernel/debug.o" "$SCRIPT_DIR/../build/kernel/memory.o" \
+    "$SCRIPT_DIR/../build/kernel/process.o"
 objcopy -O binary "$SCRIPT_DIR/../build/kernel/kernel.elf" "$SCRIPT_DIR/../build/kernel/kernel.bin"
 dd if="$SCRIPT_DIR/../build/boot/boot.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=1 conv=notrunc
 dd if="$SCRIPT_DIR/../build/boot/loader.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=5 seek=1 conv=notrunc
