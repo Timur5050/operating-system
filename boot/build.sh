f4d0ce9a6b83826e93d7ef997ec3973b67799d6d
Timur <stukantimur811@gmail.com>
2025-05-05 16:00:01 +0300

    added process compilation and adding it to the virtual disk

--- boot/build.sh
+++ boot/build.sh
@@ -23,13 +23,16 @@ gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone \
     -c "$SCRIPT_DIR/../kernel/src/memory_management/memory.c" -o "$SCRIPT_DIR/../build/kernel/memory.o"
 gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone \
     -c "$SCRIPT_DIR/../kernel/src/process/process.c" -o "$SCRIPT_DIR/../build/kernel/process.o"
+gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone \
+    -c "$SCRIPT_DIR/../kernel/src/syscalls/syscall.c" -o "$SCRIPT_DIR/../build/kernel/syscall.o"
 ld -nostdlib -T "$SCRIPT_DIR/../kernel/src/link.lds" -o "$SCRIPT_DIR/../build/kernel/kernel.elf" \
     "$SCRIPT_DIR/../build/kernel/kernel.o" "$SCRIPT_DIR/../build/kernel/main.o" \
     "$SCRIPT_DIR/../build/kernel/trapa.o" "$SCRIPT_DIR/../build/kernel/trap.o" \
     "$SCRIPT_DIR/../build/kernel/liba.o" "$SCRIPT_DIR/../build/kernel/print.o" \
     "$SCRIPT_DIR/../build/kernel/debug.o" "$SCRIPT_DIR/../build/kernel/memory.o" \
-    "$SCRIPT_DIR/../build/kernel/process.o"
+    "$SCRIPT_DIR/../build/kernel/process.o" "$SCRIPT_DIR/../build/kernel/syscall.o"
 objcopy -O binary "$SCRIPT_DIR/../build/kernel/kernel.elf" "$SCRIPT_DIR/../build/kernel/kernel.bin"
 dd if="$SCRIPT_DIR/../build/boot/boot.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=1 conv=notrunc
 dd if="$SCRIPT_DIR/../build/boot/loader.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=5 seek=1 conv=notrunc
 dd if="$SCRIPT_DIR/../build/kernel/kernel.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=100 seek=6 conv=notrunc
+dd if="$SCRIPT_DIR/../build/boot/user.bin" of="$SCRIPT_DIR/../build/boot.img" bs=512 count=10 seek=106 conv=notrunc
