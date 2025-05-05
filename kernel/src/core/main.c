cbaf61b69800d8157c5b5bd12a803fe6dfb9e108
Timur <stukantimur811@gmail.com>
2025-05-05 15:59:34 +0300

    created main for testing processes startage

--- kernel/src/core/main.c
+++ kernel/src/core/main.c
@@ -2,12 +2,14 @@
 #include "../print_function/print.h"
 #include "../memory_management/memory.h"
 #include "../process/process.h"
+#include "../syscalls/syscall.h"
 
 void KMain(void)
 { 
    init_idt();
    init_memory();  
    init_kvm();
+   init_system_call();
    init_process();
    launch();
 }
